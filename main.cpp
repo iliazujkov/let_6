6be1c93e5ed6e6ee61060c99e2a34f0cbd135448
iliazujkov <iliazujkov@gmail.com>
2021-11-05 13:27:26 +0300

    First try

---
+++ main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int main() {
+    char s[]="1234567890AaBbCcDdEeFfGgHhXxYyZz";
+    for (int i=0; i<32; i++) {
+        for (int j=0; j<32; j++) {
+            for (int k=0; k<32; k++) {
+                for (int m=0; m<32; m++) {
+                    for (int n=0; n<32; n++) {
+                           cout << s[i] << s[j] << s[k] << s[m] << s[n] << '\n';
+                       }
+                    }
+                }
+            }
+        }
+    }
+    return 0;
+}
