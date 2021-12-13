9c90cc64729ba6d3c9b7e19ca9e68005c7c9ac64
iliazujkov <90790199+iliazujkov@users.noreply.github.com>
2021-12-13 17:31:16 +0300

    removed line limit

--- main.cpp
+++ main.cpp
@@ -2,17 +2,16 @@
 #include <string>
 int main() {
     char s[]="1234567890AaBbCcDdEeFfGgHhXxYyZz";
-    for (int i=0; i<32; i++) {
-        for (int j=0; j<32; j++) {
-            for (int k=0; k<32; k++) {
-                for (int m=0; m<32; m++) {
-                    for (int n=0; n<32; n++) {
+    for (int i=0; i<sizeof(s)-1; i++) {
+        for (int j=0; j<sizeof(s)-1; j++) {
+            for (int k=0; k<sizeof(s)-1; k++) {
+                for (int m=0; m<sizeof(s)-1; m++) {
+                    for (int n=0; n<sizeof(s)-1; n++) {
                            std::cout << s[i] << s[j] << s[k] << s[m] << s[n] << '\n';
                        }
                     }
                 }
             }
         }
-    }
     return 0;
 }
