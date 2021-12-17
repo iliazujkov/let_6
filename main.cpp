f84410ffae33e3aa0bf2cad35950c4038a259a4d
iliazujkov <90790199+iliazujkov@users.noreply.github.com>
2021-12-17 13:04:45 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,12 +1,13 @@
 #include <iostream>
 #include <string>
 int main() {
-    char s[]="1234567890AaBbCcDdEeFfGgHhXxYyZz";
-    for (int i=0; i<sizeof(s)-1; i++) {
-        for (int j=0; j<sizeof(s)-1; j++) {
-            for (int k=0; k<sizeof(s)-1; k++) {
-                for (int m=0; m<sizeof(s)-1; m++) {
-                    for (int n=0; n<sizeof(s)-1; n++) {
+    const char *s="1234567890AaBbCcDdEeFfGgHhXxYyZz";
+    const size_t length = strlen(s);
+    for (int i=0; i<length; i++) {
+        for (int j=0; j<length; j++) {
+            for (int k=0; k<length; k++) {
+                for (int m=0; m<length; m++) {
+                    for (int n=0; n<length; n++) {
                            std::cout << s[i] << s[j] << s[k] << s[m] << s[n] << '\n';
                        }
                     }
